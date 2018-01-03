0c49c270c176d13c9c8fbd80481279d281cf7fd5
cursist <cursist@LOK111-05>
2018-01-03 13:36:10 +0100

    Hoofdstuk 4: deel1

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -9,8 +9,12 @@ class Main {
 			new ClassPathXmlApplicationContext("container.xml")) {
 			System.out.println(context.getBean(ClassA.class).getBoodschap());
 			System.out.println(context.getBean(InterfaceB.class).getBoodschap());
+			System.out.println(context.getBean(ClassA.class).getTelefoonNrHelpDesk());
+			ClassA objectA = context.getBean(ClassA.class);
+			System.out.println(objectA.getKoersenURL());
+			System.out.println(objectA.getDefaultTaalEnLand().getDisplayName());
+			System.out.println("Bestand bestaat: " + objectA.getImportData().exists());
+			System.out.println(objectA.getDefaultBladRichting());
 		}
-
 	}
-
 }
