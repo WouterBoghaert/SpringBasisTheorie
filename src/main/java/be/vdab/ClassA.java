1146db9f972c1439930c225635d1510d1b90a0f1
cursist <cursist@LOK111-05>
2018-01-03 12:43:44 +0100

    Hoofdstuk 3

---
+++ src/main/java/be/vdab/ClassA.java
@@ -0,0 +1,8 @@
+package be.vdab;
+
+class ClassA implements InterfaceA{
+	@Override
+	public String getBoodschap() {
+		return "ClassA object";
+	}
+}
---
+++ src/main/java/be/vdab/ClassB.java
@@ -0,0 +1,8 @@
+package be.vdab;
+
+class ClassB implements InterfaceB{
+	@Override
+	public String getBoodschap() {
+		return "ClassB object";
+	}
+}
