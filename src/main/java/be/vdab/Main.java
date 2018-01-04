b5506f9bdaf7b307f3a676503fb5273562d840ed
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2018-01-04 19:33:47 +0100

    Hoofdstuk 5

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -22,6 +22,12 @@ class Main {
 			System.out.println(context.getBean(HelpdeskMedewerkers.class));
 			System.out.println(context.getBean(ClassA.class).getWebMasterEmailAdres());
 			System.out.println(context.getBean(ClassA.class).getWebsiteGestart());
+		
+			context.getBean("teller1", Teller.class).verhoog();
+			context.getBean("teller1", Teller.class).verhoog();
+			Teller teller2 = context.getBean("teller2", Teller.class);
+			teller2.verhoog();
+			context.getBean("teller2", Teller.class).verhoog();
 		}
 	}
 }
